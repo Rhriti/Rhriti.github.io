00d2c9268fa74de1232eecff8cf8ddaa80fc8c02
Rhriti <hritijrana07@gmail.com>
2023-05-19 14:06:10 +0530

    resume added

---
+++ js.js
@@ -0,0 +1,14 @@
+TYPE="text/javascript">
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+//if IE4+
+document.onselectstart=new Function ("return false")
+//if NS6
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
